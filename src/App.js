c3aba2677aed2e7317a8eb6e95023f42cbaf2781
hue ho <hthi53503@gmail.com>
2024-05-18 16:27:24 +0700

    completed get and display all data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useEffect } from "react";
+import React, { useState } from 'react';
+import "./App.css";
+import Navbar from "./components/layout/Navbar";
+import Users from "./components/users/Users";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// App.js
+const App = () => {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+  const fetchData = async () => {
+  try {
+  const response = await axios.get("https://api.github.com/users");
+  setUsers(response.data);
+  } catch (error) {console.error("Error fetching data:", error);
 }
-
+};
+fetchData();
+}, []);
+return (
+<div className="App">
+<Navbar />
+<div className="container">
+<h1>GitHub Users Data</h1>
+</div>
+<Users users={users} />
+</div>
+);
+};
 export default App;
