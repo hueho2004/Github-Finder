0b0e73ac3762619931aad1be2834f7c6056d63c0
hue ho <hthi53503@gmail.com>
2024-05-19 16:52:27 +0700

    Display the search result

--- src/App.js
+++ src/App.js
@@ -1,30 +1,16 @@
-import axios from "axios";
-import { useEffect } from "react";
-import React, { useState } from 'react';
+// App.js
+import React from "react";
 import "./App.css";
 import Navbar from "./components/layout/Navbar";
-import Users from "./components/users/Users";
-
-// App.js
+import Search from "./components/users/Search";
 const App = () => {
-  const [users, setUsers] = useState([]);
-  useEffect(() => {
-  const fetchData = async () => {
-  try {
-  const response = await axios.get("https://api.github.com/users");
-  setUsers(response.data);
-  } catch (error) {console.error("Error fetching data:", error);
-}
-};
-fetchData();
-}, []);
 return (
 <div className="App">
 <Navbar />
 <div className="container">
 <h1>GitHub Users Data</h1>
+<Search />
 </div>
-<Users users={users} />
 </div>
 );
 };
