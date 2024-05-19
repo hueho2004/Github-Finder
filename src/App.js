000c71ee763dec7c2c84fb7257015a96cad6440e
hue ho <hthi53503@gmail.com>
2024-05-19 17:25:13 +0700

    fix search user

--- src/App.js
+++ src/App.js
@@ -5,13 +5,13 @@ import Navbar from "./components/layout/Navbar";
 import Search from "./components/users/Search";
 const App = () => {
 return (
-<div className="App">
-<Navbar />
-<div className="container">
-<h1>GitHub Users Data</h1>
-<Search />
-</div>
-</div>
-);
+  <div className="App">
+    <Navbar />
+    <div className="container">
+      <h1>GitHub Users Data</h1>
+    <Search />
+    </div>
+  </div>
+  );
 };
 export default App;
