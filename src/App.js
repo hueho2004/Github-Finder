ac5ab7212f41d62e6fa50de374f08adb815f5df7
hue ho <hthi53503@gmail.com>
2024-05-20 15:58:09 +0700

    Display all detail information to user

--- src/App.js
+++ src/App.js
@@ -5,20 +5,23 @@ import Navbar from "./components/layout/Navbar";
 import Search from "./components/users/Search";
 import About from "./components/pages/About";
 import NotFound from "./components/pages/NotFound.JS";
+import User from "./components/users/User";
 const App = () => {
   return (
     <div className="App">
-      <Router>
-        <Navbar />
-        <div className="container">
-          <Switch>
-            <Route exact path="/" component={Search} />
-            <Route exact path="/about" component={About} />
-            <Route path="/*" component={NotFound}></Route>
-          </Switch>
-        </div>
-      </Router>
-    </div>
+<Router>
+<Navbar />
+<div className="container">
+<Switch>
+<Route exact path="/" component={Search} />
+<Route exact path="/about" component={About} />
+{/* Add this line to specify the routing*/}
+<Route exact path="/user/:id" component={User} />
+<Route path="/*" component={NotFound}></Route>
+</Switch>
+</div>
+</Router>
+</div>
     );
 };
 export default App;
