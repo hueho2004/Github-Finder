8d27ee4f4d3405b1ea40a48b720448904df1a207
hue ho <hthi53503@gmail.com>
2024-05-21 00:59:58 +0700

    Fix bug

--- src/components/layout/Home.js
+++ src/components/layout/Home.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom/cjs/react-router-dom.min";
-import About from "../components/pages/About";
+import About from "../pages/About";
 import NotFound from "../components/pages/NotFound";
 import Search from "../users/Search";
 import User from "../users/User";
