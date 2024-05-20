3596d565265a7cd931dc0db21e0891c8fa02c165
hue ho <hthi53503@gmail.com>
2024-05-20 16:24:14 +0700

    fix make the App.js a bit cleaner

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import { BrowserRouter as Router } from "react-router-dom";
 import "./App.css";
 import Home from "./components/layout/Home";
 import Navbar from "./components/layout/Navbar";
+
 const App = () => {
-return (
-<div className="App">
-<Router>
-<Navbar />
-<Home />
-</Router>
-</div>
-);
+  return (
+    <div className="App">
+      <Router>
+        <Navbar />
+        <Home />
+      </Router>
+    </div>
+  );
 };
+
 export default App;
--- src/components/layout/Home.js
+++ src/components/layout/Home.js
@@ -4,16 +4,18 @@ import About from "../pages/About";
 import NotFound from "../pages/NotFound";
 import Search from "../users/Search";
 import User from "../users/User";
+
 const Home = () => {
-return (
-<div className="container">
-<Switch>
-<Route exact path="/" component={Search} />
-<Route exact path="/about" component={About} />
-<Route exact path="/user/:id" component={User} />
-<Route path="/*" component={NotFound}></Route>
-</Switch>
-</div>
-);
+  return (
+    <div className="container">
+      <Switch>
+        <Route exact path="/" component={Search} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/user/:id" component={User} />
+        <Route path="/*" component={NotFound}></Route>
+      </Switch>
+    </div>
+  );
 };
+
 export default Home;
