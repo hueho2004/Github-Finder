6f41ae3a794ff8165b3e53c4c21f1d7fc43830a7
hue ho <hthi53503@gmail.com>
2024-05-20 11:10:12 +0700

    Create basic information of the project and handle undefined route

--- src/App.js
+++ src/App.js
@@ -3,19 +3,22 @@ import "./App.css";
 import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Search from "./components/users/Search";
+import About from "./components/pages/About";
+import NotFound from "./components/pages/NotFound.JS";
 const App = () => {
-return (
+  return (
     <div className="App">
-    <Router>
-    <Navbar />
-    <div className="container">
-      <h1>GitHub Users Data</h1>
-      <Switch>
-      <Route exact path="/" component={Search} />
-      </Switch>
+      <Router>
+        <Navbar />
+        <div className="container">
+          <Switch>
+            <Route exact path="/" component={Search} />
+            <Route exact path="/about" component={About} />
+            <Route path="/*" component={NotFound}></Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
-    </Router>
-  </div>
-  );
+    );
 };
 export default App;
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,18 +1,18 @@
-// /src/components/layout/Navbar.js
 import React from "react";
+import { Link } from "react-router-dom";
 const Navbar = () => {
-return (
-<nav className="navbar bg-success">
-<h1>
-<i className="fab fa-github" /> GitHub Finder
-</h1>
-<ul>
-<li>
-<a href="#>">Home</a>
-<a href="#>">About</a>
-</li>
-</ul>
-</nav>
-);
+    return (
+        <nav className="navbar bg-success">
+            <h1>
+                <i className="fab fa-github" /> GitHub Finder
+            </h1>
+            <ul>
+                <li>
+                <Link to="/">Home</Link>
+                <Link to="/about">About</Link>
+                </li>
+            </ul>
+        </nav>
+    );
 };
 export default Navbar;
