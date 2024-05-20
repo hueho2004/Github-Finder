903ef30ca59b5e3f4fcb8dcfce665427d3d5bd05
hue ho <hthi53503@gmail.com>
2024-05-20 16:19:22 +0700

    Make the App.js a bit cleaner

--- src/App.js
+++ src/App.js
@@ -1,27 +1,16 @@
 import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
 import "./App.css";
-import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
+import Home from "./components/layout/Home";
 import Navbar from "./components/layout/Navbar";
-import Search from "./components/users/Search";
-import About from "./components/pages/About";
-import NotFound from "./components/pages/NotFound.JS";
-import User from "./components/users/User";
 const App = () => {
-  return (
-    <div className="App">
+return (
+<div className="App">
 <Router>
 <Navbar />
-<div className="container">
-<Switch>
-<Route exact path="/" component={Search} />
-<Route exact path="/about" component={About} />
-{/* Add this line to specify the routing*/}
-<Route exact path="/user/:id" component={User} />
-<Route path="/*" component={NotFound}></Route>
-</Switch>
-</div>
+<Home />
 </Router>
 </div>
-    );
+);
 };
 export default App;
